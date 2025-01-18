5b77c335cc36944703f301d80b3e5bf329bbbe19
soushimiya <131571752+soushimiya@users.noreply.github.com>
2025-01-18 15:08:26 +0900

    wip

---
+++ source/window/Main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_file_dialog_file_selected(path: String) -> void:
+	var js = JSONMinifier.minify_json(FileAccess.get_file_as_string(path))
+	JSON.parse_string(js)
